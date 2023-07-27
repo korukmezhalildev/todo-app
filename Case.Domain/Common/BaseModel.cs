0c3c718b96fcc82ea35a2b62a24ac805d9e55193
halilkorukmez <korukmez.dev@outlook.com>
2023-07-28 02:58:15 +0300

    h

--- Case.Domain/Common/BaseModel.cs
+++ Case.Domain/Common/BaseModel.cs
@@ -2,8 +2,10 @@ namespace Case.Domain.Common;
 
 public abstract class BaseModel
 {
+
     public required Guid Id { get; set; } = new Guid();
     public required DateTime CreatedDate { get; set; } = DateTime.Now;
     public required bool IsActive { get; set; } = true;
     public DateTime? UpdatedDate { get; set; }
+
 }
